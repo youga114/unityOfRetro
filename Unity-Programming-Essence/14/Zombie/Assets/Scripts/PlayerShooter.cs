c2a34f4fb8b979a2555c0d88673b8175b3c11d8d
kimyujun <kimyujun@the8games.com>
2022-04-11 19:55:40 +0900

    [782p] Add code

--- Unity-Programming-Essence/14/Zombie/Assets/Scripts/PlayerShooter.cs
+++ Unity-Programming-Essence/14/Zombie/Assets/Scripts/PlayerShooter.cs
@@ -29,6 +29,19 @@ public class PlayerShooter : MonoBehaviour {
 
     private void Update() {
         // 입력을 감지하고 총 발사하거나 재장전
+        if (playerInput.fire)
+        {
+            gun.Fire();
+        }
+        else if (playerInput.reload)
+        {
+            if (gun.Reload())
+            {
+                playerAnimator.SetTrigger("Reload");
+            }
+        }
+        
+        UpdateUI();
     }
 
     // 탄약 UI 갱신
@@ -41,7 +54,20 @@ public class PlayerShooter : MonoBehaviour {
     }
 
     // 애니메이터의 IK 갱신
-    private void OnAnimatorIK(int layerIndex) {
+    private void OnAnimatorIK(int layerIndex)
+    {
+        gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
         
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+        
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation);
     }
 }
