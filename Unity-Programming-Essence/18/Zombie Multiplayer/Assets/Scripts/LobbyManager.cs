a7ad18496cfca2af10d1383f11cc4ff6fadf5a28
kimyujun <kimyujun@the8games.com>
2022-04-21 15:22:30 +0900

    [Unity] Add last commit

--- Unity-Programming-Essence/18/Zombie Multiplayer/Assets/Scripts/LobbyManager.cs
+++ Unity-Programming-Essence/18/Zombie Multiplayer/Assets/Scripts/LobbyManager.cs
@@ -11,32 +11,59 @@ public class LobbyManager : MonoBehaviourPunCallbacks {
     public Button joinButton; // 룸 접속 버튼
 
     // 게임 실행과 동시에 마스터 서버 접속 시도
-    private void Start() {
-        
+    private void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        joinButton.interactable = false;
+        connectionInfoText.text = "마스터 서버에 접속 중...";
     }
 
     // 마스터 서버 접속 성공시 자동 실행
-    public override void OnConnectedToMaster() {
-        
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "온라인 : 마스터 서버와 연결됨";
     }
 
     // 마스터 서버 접속 실패시 자동 실행
-    public override void OnDisconnected(DisconnectCause cause) {
-        
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
+
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // 룸 접속 시도
-    public void Connect() {
-        
+    public void Connect()
+    {
+        joinButton.interactable = false;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "룸에 접속...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
     // (빈 방이 없어)랜덤 룸 참가에 실패한 경우 자동 실행
-    public override void OnJoinRandomFailed(short returnCode, string message) {
-        
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "빈 방이 없음, 새로운 방 생성...";
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 4});
     }
 
     // 룸에 참가 완료된 경우 자동 실행
-    public override void OnJoinedRoom() {
-        
+    public override void OnJoinedRoom()
+    {
+        connectionInfoText.text = "방 참가 성공";
+        PhotonNetwork.LoadLevel("Main");
     }
 }
